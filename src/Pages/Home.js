21c9511e645f61b68dd93c6949ba8bedb4d9bbd9
vanshika-37 <vanshikasood77@gmail.com>
2024-09-04 16:20:39 +0530

    add styling on landing page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,26 +1,34 @@
+import React, { useState, useEffect } from "react";
 import Header from "./Header.js";
-import { useState, useEffect } from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 export default function Home() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+  useEffect(() => {
+    setTitle('EzPay');
+    setContent('A Secure and Efficient Digital Payment Solution');
+  }, []);
 
-
-    useEffect(()=>{
-        setTitle('EzPay');
-        setContent('A Secure and Efficient Digital Payment Solution');
-    },[])
-
-    return (
-        <div>
-        <Header />
-        <main className="hero-section">
-          <div className="hero-content">
-            <h1>Welcome to {title}</h1>
-            <p>{content}</p>
-          </div>
-        </main>
-      </div>
-    )
+  return (
+    <div>
+      <Header />
+      <main className="hero-section mt-5">
+        <Container>
+          <Row className="align-items-center justify-content-center">
+            <Col md={7} className="text-center text-md-start">
+              <h1>Welcome to {title}</h1>
+              <p>{content}</p>
+            </Col>
+            <Col className="text-center">
+              <Image src="/heroIllustration.svg" alt="EzPay Illustration" fluid className="home-illustration" />
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    </div>
+  );
 }
+
+
