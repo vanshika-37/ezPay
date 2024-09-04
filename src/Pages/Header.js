2d647dfc428f4bd97bb2356872e8e7bb82cc7523
vanshika-37 <vanshikasood77@gmail.com>
2024-09-04 22:33:17 +0530

    improve styling

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -4,22 +4,24 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
-        <Navbar expand="lg">
-            <Container>
-                <Navbar.Brand href="/">
-                    <h1><i><b>EzPay</b></i></h1>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-                    <Nav className="header-navbar-items">
-                        <Nav.Link as={NavLink} to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="/balance" className={({ isActive }) => isActive ? "active" : ""}>Balance</Nav.Link>
-                        <Nav.Link as={NavLink} to="/payment" className={({ isActive }) => isActive ? "active" : ""}>Payment</Nav.Link>
-                        <Nav.Link as={NavLink} to="/help" className={({ isActive }) => isActive ? "active" : ""}>Help</Nav.Link>
-                        <Nav.Link as={NavLink} to="/profile" className={({ isActive }) => isActive ? "active" : ""}>Profile</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div className="header">
+            <Navbar expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <h1><i><b>EzPay</b></i></h1>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+                        <Nav className="header-navbar-items">
+                            <Nav.Link as={NavLink} to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</Nav.Link>
+                            <Nav.Link as={NavLink} to="/balance" className={({ isActive }) => isActive ? "active" : ""}>Balance</Nav.Link>
+                            <Nav.Link as={NavLink} to="/payment" className={({ isActive }) => isActive ? "active" : ""}>Payment</Nav.Link>
+                            <Nav.Link as={NavLink} to="/help" className={({ isActive }) => isActive ? "active" : ""}>Help</Nav.Link>
+                            <Nav.Link as={NavLink} to="/profile" className={({ isActive }) => isActive ? "active" : ""}>Profile</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,20 +13,23 @@ export default function Home() {
 
   return (
     <div>
-      <Header />
-      <main className="hero-section mt-5">
-        <Container>
-          <Row className="align-items-center justify-content-center">
-            <Col md={7} className="text-center text-md-start">
-              <h1>Welcome to {title}</h1>
-              <p>{content}</p>
-            </Col>
-            <Col className="text-center">
-              <Image src="/heroIllustration.svg" alt="EzPay Illustration" fluid className="home-illustration" />
-            </Col>
-          </Row>
-        </Container>
-      </main>
+      <div className="home-page-component d-flex flex-column justify-content-center">
+        <Header />
+        <main className="hero-section d-flex">
+          <Container >
+            <Row className="align-items-center justify-content-center">
+              <Col md={7} className="text-center text-md-start">
+                <h1>Welcome to, {title}</h1>
+                <p>{content}</p>
+              </Col>
+              <Col className="text-center">
+                <Image src="/heroIllustration.svg" alt="EzPay Illustration" fluid className="home-illustration" />
+              </Col>
+            </Row>
+          </Container>
+        </main >
+      </div>
+
     </div>
   );
 }
