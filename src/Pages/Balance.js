04b51ef9fe5a6617189d169967c272acd61edec6
vanshika-37 <vanshikasood77@gmail.com>
2024-09-05 00:02:07 +0530

    Integrate backend API except for Help Component

--- src/Pages/Balance.js
+++ src/Pages/Balance.js
@@ -1,5 +1,19 @@
+import { useState, useEffect } from "react";
 import PageTemplate from "./PageTemplate";
 
 export default function Balance() {
-    return <PageTemplate title="Balance" />;
+    const [title, setTitle] = useState('');
+
+    useEffect(() => {
+        const fetchTitle = async () => {
+            const response = await fetch('http://localhost:8090/api/support/balance');
+            const data = await response.text();
+            setTitle(data);
+        };
+    
+        fetchTitle();
+    }, []);
+    
+
+    return <PageTemplate title={title} />;
 }
--- src/Pages/Payment.js
+++ src/Pages/Payment.js
@@ -1,5 +1,19 @@
+import { useState, useEffect } from "react";
 import PageTemplate from "./PageTemplate";
 
-export default function Payment() {
-    return <PageTemplate title="Payment" />;
+export default function Balance() {
+    const [title, setTitle] = useState('');
+
+    useEffect(() => {
+        const fetchTitle = async () => {
+            const response = await fetch('http://localhost:8090/api/support/payment');
+            const data = await response.text();
+            setTitle(data);
+        };
+    
+        fetchTitle();
+    }, []);
+    
+
+    return <PageTemplate title={title} />;
 }
