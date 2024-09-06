a345ff658dad2838c7ea159b38f105f802fd4d85
vaishnave3 <vaishnavejp3@gmail.com>
2024-09-06 16:55:17 +0530

    added ticket card

--- src/Pages/Help.js
+++ src/Pages/Help.js
@@ -16,6 +16,30 @@ export default function Help() {
         setUserTickets(json);
     }
 
+    const resolveTicket = async(ticketId) => {
+        let response = await fetch(base + `/resolveticket/${ticketId}`, {"method":"PUT"});
+        let json = await response.json();
+
+        setUserTickets(prevUserTicket => 
+            prevUserTicket.map(ticket => 
+                ticket.ticketId === ticketId ? {...ticket, status: json.status} : ticket
+            )
+        )
+
+        console.log("Response from Resolve endpoint :", json);
+    }
+
+    const deleteTicket = async(ticketId) => {
+        let response = await fetch(base + `/delete/${ticketId}`, {"method": "DELETE"});
+        let json = await response.json();
+        handleDelete(ticketId);
+        console.log("Response from Delete endpoint :", json);
+    }
+
+    const handleDelete = (id) => {
+        setUserTickets(userTickets.filter(ticket => ticket.ticketId !== id));
+    }
+
     useEffect(() => {
         getUserTickets();
     }, [])
@@ -29,14 +53,22 @@ export default function Help() {
             <div className="help-header">
                 <Header />
             </div>
-            <div className="help-cards">
-                {
-                    userTickets.map((ticket, key) => {
-                        return <TicketCard 
-                                    ticket = {ticket}
-                                />
-                    })
-                }
+            <div className="help-body">
+                <div className="help-title">
+                    <h2>TICKETS</h2>
+                    <button className="help-create-btn">Create Ticket</button>
+                </div>
+                <div className="help-cards">
+                    {
+                        userTickets.map((ticket, key) => {
+                            return <TicketCard 
+                                        ticket = {ticket}
+                                        resolveHandler = {resolveTicket}
+                                        deleteHandler = {deleteTicket}
+                                    />
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
--- src/Pages/TicketCard.js
+++ src/Pages/TicketCard.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
 
 export default function TicketCard(props) {
-  return (
-    <div className='ticket-cards'>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </div>
-  )
+
+    const [show, setShow] = useState(false);
+    const handleModalShow = () => {
+        setShow(!show);
+    }
+
+    return (
+        <div className='ticket-cards'>
+            <Card className='card' onClick={handleModalShow}>
+                <Card.Body className='card-body'>
+                    <Card.Title>
+                        #{props.ticket.ticketId}
+                    </Card.Title>
+                    <Card.Text>
+                        <h5>{props.ticket.issueDescription}</h5>
+                        <p>Status: {props.ticket.status}</p>
+                    </Card.Text>
+                    <div className='card-btns'>
+                        <Button 
+                            variant={props.ticket.status == 'OPEN' ? 'success' : 'primary'} 
+                            onClick={() => props.resolveHandler(props.ticket.ticketId)}
+                        >
+                            {props.ticket.status == 'OPEN' ? <p>RESOLVE</p> : <p>UNRESOLVE</p>}
+                        </Button>
+
+                        <Button 
+                            variant="danger"
+                            onClick={() => props.deleteHandler(props.ticket.ticketId)}
+                        >
+                            Delete Ticket
+                        </Button>
+                    </div>
+                </Card.Body>
+            </Card>
+
+
+            <Modal show={show} onHide={handleModalShow}>
+                <Modal.Header closeButton>
+                <Modal.Title>{props.ticket.issueDescription}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Created Date: {props.ticket.dateCreated}</p>
+                    {props.ticket.status == 'OPEN' ? null : <p>Resolved Date: {props.ticket.dateResolved}</p>}
+                    <p>Status: {props.ticket.status}</p>
+                </Modal.Body>
+            </Modal>
+        </div>
+    )
 }
